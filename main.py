d22dc48ca4ab9d2bb34de49253d05ee9c477ad6c
SW1pr0g <poryadin_alex@mail.ru>
2021-12-01 15:18:38 -0600

    Add v0.2

--- main.py
+++ main.py
@@ -1,36 +1,76 @@
 import pygame
 
 pygame.init()
+
 clock = pygame.time.Clock()
 fps = 120
 
-#окно
 bottom_panel = 150
 screen_width = 800
-screen_height = 400
+screen_height = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('RPGbySW1pr0g')
+pygame.display.set_caption('Battle')
 
-#ИЗОБРАЖЕНИЯ
-#фон
+
+#подгрузка изображений
 background_img = pygame.image.load('img/Background/background.png').convert_alpha()
-#
+#panel image
 panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
 
+
+#задний фон
 def draw_bg():
     screen.blit(background_img, (0, 0))
 
+
+#панелька
 def draw_panel():
     screen.blit(panel_img, (0, screen_height - bottom_panel))
+
+
+
+#боец
+class Fighter():
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        img = pygame.image.load(f'img/{self.name}/Idle/0.png')
+        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
+knight = Fighter(200, 260, 'Knight', 30, 10, 3)
+bandit1 = Fighter(550, 270, 'Bandit', 20, 6, 1)
+bandit2 = Fighter(700, 270, 'Bandit', 20, 6, 1)
+
+bandit_list = []
+bandit_list.append(bandit1)
+bandit_list.append(bandit2)
+
 
 run = True
 while run:
 
     clock.tick(fps)
-
+   
     draw_bg()
-    draw_panel()
+    draw_panel()   
+    knight.draw()
+
+    for bandit in bandit_list:
+        bandit.draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -38,4 +78,3 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
