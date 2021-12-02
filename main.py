7b7398ff9001d5132eb7ce5f5ecef17fe4ca5613
SW1pr0g <poryadin_alex@mail.ru>
2021-12-02 08:55:55 -0600

    Add v0.3

--- main.py
+++ main.py
@@ -5,32 +5,47 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 120
 
+#игра-окно
 bottom_panel = 150
 screen_width = 800
 screen_height = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Battle')
+pygame.display.set_caption('RPGbySW1pr0g')
 
 
-#подгрузка изображений
+#шрифт
+font = pygame.font.SysFont('Times New Roman', 26)
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+#ИЗОБРАЖЕНИЯ
 background_img = pygame.image.load('img/Background/background.png').convert_alpha()
-#panel image
+#панелька
 panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
 
 
-#задний фон
+#текст
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
 def draw_bg():
     screen.blit(background_img, (0, 0))
 
 
-#панелька
 def draw_panel():
     screen.blit(panel_img, (0, screen_height - bottom_panel))
+    draw_text(f'{knight.name} HP: {knight.hp}', font, red, 100, screen_height - bottom_panel + 10)
+    for count, i in enumerate(bandit_list):
+        draw_text(f'{i.name} HP: {i.hp}', font, red, 550, (screen_height - bottom_panel + 10) + count * 60)
 
 
 
-#боец
+
+#БОЕЦ
 class Fighter():
     def __init__(self, x, y, name, max_hp, strength, potions):
         self.name = name
@@ -40,14 +55,61 @@ class Fighter():
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        img = pygame.image.load(f'img/{self.name}/Idle/0.png')
-        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0#0:idle, 1:attack, 2:hurt, 3:dead
+        self.update_time = pygame.time.get_ticks()
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #атака
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
 
+    def update(self):
+        animation_cooldown = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
     def draw(self):
         screen.blit(self.image, self.rect)
+
+
+
+class HealthBar():
+    def __init__(self, x, y, hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+
+
+    def draw(self, hp):
+        #новое хп
+        self.hp = hp
+        #хп
+        ratio = self.hp / self.max_hp
+        pygame.draw.rect(screen, red, (self.x, self.y, 150, 20))
+        pygame.draw.rect(screen, green, (self.x, self.y, 150 * ratio, 20))
+
+
 
 
 knight = Fighter(200, 260, 'Knight', 30, 10, 3)
@@ -58,17 +120,27 @@ bandit_list = []
 bandit_list.append(bandit1)
 bandit_list.append(bandit2)
 
+knight_health_bar = HealthBar(100, screen_height - bottom_panel + 40, knight.hp, knight.max_hp)
+bandit1_health_bar = HealthBar(550, screen_height - bottom_panel + 40, bandit1.hp, bandit1.max_hp)
+bandit2_health_bar = HealthBar(550, screen_height - bottom_panel + 100, bandit2.hp, bandit2.max_hp)
+
 
 run = True
 while run:
 
     clock.tick(fps)
-   
-    draw_bg()
-    draw_panel()   
-    knight.draw()
 
+    draw_bg()
+
+    draw_panel()
+    knight_health_bar.draw(knight.hp)
+    bandit1_health_bar.draw(bandit1.hp)
+    bandit2_health_bar.draw(bandit2.hp)
+
+    knight.update()
+    knight.draw()
     for bandit in bandit_list:
+        bandit.update()
         bandit.draw()
 
     for event in pygame.event.get():
